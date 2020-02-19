7baa1a88e9af15b20fecc370ca7711317f1676b4
takegata jumpei <takegata.j@gmail.com>
2020-02-19 21:42:48 +0900

    add jest

---
+++ tests/game.test.ts
@@ -0,0 +1,47 @@
+import Game from '#/js/game';
+import 'jest-canvas-mock';
+
+describe('game', (): void => {
+  test('always ok', (): void => {
+    const txt = 'game';
+    expect(txt).toBe('game');
+    const c = document.createElement('canvas');
+    const game = new Game(c);
+    expect(game.isGaming).toBe(false);
+
+    // 'コンストラクト'
+    // '初期の配置'
+    // '勝敗'
+    // '裏返り'
+    // '初期におけるところ'
+  });
+  test('初期の配置', (): void => {
+    const c = document.createElement('canvas');
+    const game = new Game(c);
+    game.reset();
+    expect(game.cells[3][3].stone).toBe('light');
+    expect(game.cells[3][4].stone).toBe('dark');
+    expect(game.cells[4][3].stone).toBe('dark');
+    expect(game.cells[4][4].stone).toBe('light');
+
+    // '勝敗'
+    // '裏返り'
+    // '初期におけるところ'
+  });
+  // game start
+  test('isStart', (): void => {
+    const c = document.createElement('canvas');
+    const game = new Game(c);
+    expect(game.isGaming).toBe(false);
+    game.start();
+    expect(game.isGaming).toBe(true);
+  });
+  // click
+
+  // '勝敗'
+  // '裏返り'
+  // '初期におけるところ'
+  // 黒番から始まる
+  // ここは置けるよね
+  // ひっくり返ったか
+});
